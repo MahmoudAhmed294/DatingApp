17836a0b460891911cad2cd15554f7e68a159b36
MahmoudAhmed294 <mahmoud.ahmad@itdeg.com>
2023-02-22 16:58:25 +0200

    adding validation for forms

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { NgxSpinnerModule } from 'ngx-spinner';
@@ -20,8 +21,9 @@ import { FileUploadModule } from 'ng2-file-upload';
         NgxSpinnerModule.forRoot({
             type: 'line-scale-party'
         }),
-        FileUploadModule
+        FileUploadModule,
+        BsDatepickerModule.forRoot()
     ],
-    exports: [BsDropdownModule, ToastrModule, TabsModule, NgxGalleryModule, NgxSpinnerModule ,FileUploadModule]
+    exports: [BsDropdownModule, ToastrModule, TabsModule, NgxGalleryModule, NgxSpinnerModule ,FileUploadModule ,BsDatepickerModule]
 })
 export class SharedModule {}
